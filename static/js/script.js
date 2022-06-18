954a4c882265a22304a053cbf9e81645dec5ff6d
sirdamis <adedejidamisola@gmail.com>
2022-06-18 01:10:22 +0100

    Inplemented the dashboard mobule scroll

--- static/js/script.js
+++ static/js/script.js
@@ -47,38 +47,38 @@ if(window.location.pathname == '/') {
 }
 
 
-// let currentScrollPositon = 0
-// let scrollAmount =320
-// const sCont = document.querySelector(".menu-icons-container")
-// const hScroll = document.querySelector(".menu-bar")
-// const btnScrollLeft =  document.querySelector("#btn-scroll-left")
-// const btnScrollRight =  document.querySelector("#btn-scroll-right")
-// btnScrollLeft.style.opacity = "0"
+let currentScrollPositon = 0
+let scrollAmount = 320
+const sCont = document.querySelector(".menu-icons-container")
+const hScroll = document.querySelector(".menu-bar")
+const btnScrollLeft =  document.querySelector("#btn-scroll-left")
+const btnScrollRight =  document.querySelector("#btn-scroll-right")
+btnScrollLeft.style.opacity = "0"
 
-// let maxScroll = -sCont.offsetWidth + hScroll.offsetWidth;
+let maxScroll = -sCont.offsetWidth + hScroll.offsetWidth;
 
-// function scrollHorizontally(val){
-//     currentScrollPositon += (val * scrollAmount)
+function scrollHorizontally(val){
+    currentScrollPositon += (val * scrollAmount)
 
-//     if(currentScrollPositon >= 0){
-//         currentScrollPositon = 0
-//         btnScrollLeft.style.opacity = "0"
-//     }
+    if(currentScrollPositon >= 0){
+        currentScrollPositon = 0
+        btnScrollLeft.style.opacity = "0"
+    }
 
-//     if(currentScrollPositon >= 0){
-//         currentScrollPositon = 0
-//         btnScrollLeft.style.opacity = "0"
-//     }else{
-//         btnScrollLeft.style.opacity = "1"
-//     }
-//     if(currentScrollPositon <= maxScroll){
-//         currentScrollPositon = maxScroll
-//         btnScrollRight.style.opacity = "0"
-//     }else{
-//         btnScrollRight.style.opacity = "1"
-//     }
-//     sCont.style.left = currentScrollPositon+"px"
-// }
+    if(currentScrollPositon >= 0){
+        currentScrollPositon = 0
+        btnScrollLeft.style.opacity = "0"
+    }else{
+        btnScrollLeft.style.opacity = "1"
+    }
+    if(currentScrollPositon <= maxScroll){
+        currentScrollPositon = maxScroll
+        btnScrollRight.style.opacity = "0"
+    }else{
+        btnScrollRight.style.opacity = "1"
+    }
+    sCont.style.left = currentScrollPositon+"px"
+}
 
 
 
